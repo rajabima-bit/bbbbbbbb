4e5d29602438e985e98bc325bdbc6645cb41d726
rajabima-bit <rjabima@gmail.com>
2026-04-13 23:50:17 -0800

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// MENU MOBILE
+// MENU
 const toggle = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav");
 
@@ -6,27 +6,31 @@ toggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
 
+// DARK MODE
+const darkBtn = document.getElementById("darkModeToggle");
+
+darkBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
 // SCROLL ANIMATION
 const elements = document.querySelectorAll('.fade-in');
 
 window.addEventListener('scroll', () => {
     elements.forEach(el => {
-        const pos = el.getBoundingClientRect().top;
-        if (pos < window.innerHeight - 100) {
+        if (el.getBoundingClientRect().top < window.innerHeight - 100) {
             el.classList.add('show');
         }
     });
 });
 
-// SMOOTH SCROLL
+// SCROLL
 function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({
-        behavior: 'smooth'
-    });
+    document.getElementById(id).scrollIntoView({behavior: 'smooth'});
 }
 
 // FORM
 document.getElementById("contactForm").addEventListener("submit", function(e){
     e.preventDefault();
-    alert("Pesan berhasil dikirim 🚀");
+    alert("Pesan terkirim 🚀");
 });
